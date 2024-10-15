3ac687a3d1dc516796a15ef4beb0e3a46948f78d
Rishikesh <rishikeshpurohit94@gmail.com>
2024-10-15 10:49:29 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Ecommerce-project/', // Use the correct subdirectory for your GitHub Pages
-  plugins: [react()],
-});
